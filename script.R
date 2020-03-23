67b50f1f65b846cd86cf84ab6a15b8bb13f44a25
Yevheniia Drozdova <evgesha.drozdova@gmail.com>
2020-03-23 13:43:03 +0200

    з дня першої смерті

--- script.R
+++ script.R
@@ -5,40 +5,51 @@ library(ggplot2)
 
 Sys.setlocale("LC_TIME", "Ukrainian")
 
-original = read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")  
+original_cases = read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")  
+original_deaths = read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv") 
+  
+deaths = gather(original_deaths, "date", "amount", starts_with("X")) %>% 
+  mutate(date = sub("X", "", date)) %>% 
+  mutate(date = as.Date(date, "%m.%d.%y")) %>% 
+  group_by(Country.Region, date) %>% 
+  mutate(deaths = sum(amount)) %>% 
+  ungroup() %>% 
+  select(Country.Region, date, deaths) %>% 
+  unique()
+
+cases = gather(original_cases, "date", "amount", starts_with("X")) %>% 
+  mutate(date = sub("X", "", date)) %>% 
+  mutate(date = as.Date(date, "%m.%d.%y")) %>% 
+  group_by(Country.Region, date) %>% 
+  mutate(cases = sum(amount)) %>% 
+  ungroup() %>% 
+  select(Country.Region, date, cases) %>% 
+  unique()
+
+data = merge(cases, deaths,  by=c("date", "Country.Region")) %>% 
+  filter(deaths > 0) %>% 
+  group_by(Country.Region) %>% 
+  arrange(Country.Region, date) %>% 
+  mutate(counter=row_number()) %>% 
+  mutate(place = Country.Region)
 
 # перелік країн, які цікаві
 target_countries = c("Austria", "Belgium", "Poland", "United Kingdom", 
                      "Belgium", "Croatia", "Czechia", "Denmark", "France", 
                      "Germany", "Greece", "Italy", "Spain", "Romania", 
                      "Sweden", "Switzerland", "US", "Netherlands", "Israel", 
-                     "Russia")
-
-data = gather(original, "date", "amount", starts_with("X")) %>% 
-  mutate(date = sub("X", "", date)) %>% 
-  mutate(date = as.Date(date, "%m.%d.%y")) 
+                     "Russia", "Ukraine")
 
 end_date = max(data$date)
 
-data = data %>% 
-  filter(amount >= 100) %>% 
-  group_by(Country.Region, date) %>% 
-  mutate(total = sum(amount)) %>% 
-  ungroup() %>% 
-  select(Country.Region, date, total) %>% 
-  unique() %>% 
-  group_by(Country.Region) %>% 
-  arrange(Country.Region, date) %>% 
-  #беремо за перший день найпершу наявну дату і далі кожен наступний день
-  mutate(counter=row_number()) %>% 
-  mutate(place = Country.Region)
-
-filtered = data %>% 
+filtered = data %>%  
   filter(Country.Region %in% target_countries)
   
+options(scipen=10000)
+
 # сірими лініями поки всі країни без фільтру по цікавим
-ggplot(filtered, aes(y = total, x = counter)) + 
-  geom_line(data = transform(data, Country.Region = NULL), mapping = aes(group = place), colour = "#696969", size=0.2)+
+ggplot(filtered, aes(y = cases, x = counter)) + 
+  geom_line(data = transform(filtered, Country.Region = NULL), mapping = aes(group = place), colour = "#696969", size=0.2)+
   geom_line(aes(group = Country.Region), size=1, colour = "#cf1e25")+
   scale_size_manual(values = c(0.2, 1))+
   facet_wrap(~ Country.Region) +
