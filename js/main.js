83ebd13105be260896f810ca7f1799bde3dc653e
Yevheniia Drozdova <evgesha.drozdova@gmail.com>
2020-03-27 21:16:52 +0200

    header та footer

--- js/main.js
+++ js/main.js
@@ -9,71 +9,13 @@ const translated_countries = ["Україна", "Австрія", "Болгар�
 var formatTime = d3.timeFormat("%d-%m-%Y");
 d3.select("#today").html(formatTime(new Date));
 
-
 Promise.all([
     d3.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"),
     d3.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
 ]).then(function(files) {
-
-    const reshape = function(df, value_col_title){
-        var filtered = df.filter(function(d) { return target_countries.includes(d["Country/Region"]) });
-        var long_data = [];
-
-        filtered.forEach( function(row) {
-            Object.keys(row).forEach( function(colname) {
-                // Ignore 'State' and 'Value' columns
-                if(colname == "Country/Region" || colname == "Province/State" || colname == "Lat" || colname == "Long") {
-                    return
-                }
-                long_data.push({"country": row["Country/Region"], [value_col_title]: +row[colname], "date": colname});
-            });
-        });
-
-        //  групуємо за країною і датою в сумуємо значення
-        var helper = {};
-        var data = long_data.reduce(function(r, o) {
-            var key = o.country + '-' + o.date;
-            if(!helper[key]) {
-                helper[key] = Object.assign({}, o); // create a copy of o
-                r.push(helper[key]);
-            } else {
-                helper[key][value_col_title] += o[value_col_title];
-            }
-            return r;
-        }, []);
-
-        return data
-    };
-
+ 
     var cases = reshape(files[0], "cases");
-    var deaths = reshape(files[1], "deaths");
-
-    /* merge */
-    function leftJoin(left, right, left_id, right_id, col_to_join) {
-        var result = [];
-        _.each(left, function (litem) {
-            var f = _.filter(right, function (ritem) {
-                    return ritem[right_id] == litem[left_id] && ritem["date"] == litem["date"];
-            });
-            if (f.length == 0) {
-                f = [{}];
-            }
-            _.each(f, function (i) {
-                var newObj = {};
-                _.each(litem, function (v, k) {
-                    newObj[k] = v;
-                });
-                _.each(i, function (v, k) {
-                    if(k == col_to_join) {
-                        newObj[k] = v;
-                    }
-                });
-                result.push(newObj);
-            });
-        });
-        return result;
-    }
-
+    var deaths = reshape(files[1], "deaths");   
 
     var mydata = leftJoin(cases, deaths, "country", "country", "deaths")
         .filter(function(d){ return d.deaths > 0  });
@@ -94,39 +36,31 @@ Promise.all([
         }
     });
 
-
-    const width = document.getElementById("chart_wrapper").getBoundingClientRect().width;
-    const columns = Math.floor(width/250);
-    if(width > 800) {
-        d3.selectAll("#chart, #chart_wrapper p, #chart_wrapper h3").style("width", columns * 250 + "px");
-    } else {
-        d3.selectAll("#chart").style("width", columns * 250 + "px");
-        d3.selectAll("#chart_wrapper p, #chart_wrapper h3").style("width", "100%");
-    }
-
-
-    d3.select(window).on("resize", function(){
+    const set_size = function(){
         const width = document.getElementById("chart_wrapper").getBoundingClientRect().width;
-        const columns = Math.floor(width / 250);
+        const columns = Math.floor(width/250);
         if(width > 800) {
             d3.selectAll("#chart, #chart_wrapper p, #chart_wrapper h3").style("width", columns * 250 + "px");
         } else {
             d3.selectAll("#chart").style("width", columns * 250 + "px");
             d3.selectAll("#chart_wrapper p, #chart_wrapper h3").style("width", "100%");
         }
-    });
+    };
+    
+    set_size();
+    d3.select(window).on("resize", set_size);
 
     const yScale = d3.scaleSymlog()
         .domain([0, 100000])
         .range([150, 0]);
 
     var xScale = d3.scaleLinear()
-        .domain([0, d3.max(mydata, function(d){ return d.index})]) // input
+        .domain([0, d3.max(mydata, function(d){ return d.index})]) 
         .range([0, 150]);
 
     var line = d3.line()
-        .x(function(d, i) { return xScale(d.index); }) // set the x values for the line generator
-        .y(function(d) { return yScale(d.cases); }); // set the y values for the line generator
+        .x(function(d, i) { return xScale(d.index); }) 
+        .y(function(d) { return yScale(d.cases); }); 
 
     var nested = d3.nest()
         .key(function(d){ return d.country; })
@@ -135,6 +69,7 @@ Promise.all([
     const height = nested.length / columns * 250;
 
     const chart_container = d3.select("#chart");
+    
     const multiple = chart_container.selectAll("svg")
             .data(nested)
             .enter()
@@ -149,12 +84,12 @@ Promise.all([
     multiple.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + 150 + ")")
-        .call(d3.axisBottom(xScale).tickValues([10, 20, 30, 40, 50, 60])); // Create an axis component with d3.axisBottom
+        .call(d3.axisBottom(xScale).tickValues([10, 20, 30, 40, 50, 60])); 
 
 
     multiple.append("g")
         .attr("class", "y axis")
-        .call(d3.axisLeft(yScale).ticks(5).tickValues([0, 10, 100, 1000, 10000, 100000])); // Create an axis component with d3.axisLeft
+        .call(d3.axisLeft(yScale).ticks(5).tickValues([0, 10, 100, 1000, 10000, 100000]));
 
 
     multiple.append("text")
@@ -188,6 +123,62 @@ Promise.all([
             })
             .attr("d", line);
     }
-
-
 });
+
+/* from width to long format */
+const reshape = function(df, value_col_title){
+    var filtered = df.filter(function(d) { return target_countries.includes(d["Country/Region"]) });
+    var long_data = [];
+
+    filtered.forEach( function(row) {
+        Object.keys(row).forEach( function(colname) {
+            // columns for ignore
+            if(colname == "Country/Region" || colname == "Province/State" || colname == "Lat" || colname == "Long") {
+                return
+            }
+            long_data.push({"country": row["Country/Region"], [value_col_title]: +row[colname], "date": colname});
+        });
+    });
+
+    //  групуємо за країною і датою в сумуємо значення
+    var helper = {};
+    var data = long_data.reduce(function(r, o) {
+        var key = o.country + '-' + o.date;
+        if(!helper[key]) {
+            helper[key] = Object.assign({}, o); // create a copy of o
+            r.push(helper[key]);
+        } else {
+            helper[key][value_col_title] += o[value_col_title];
+        }
+        return r;
+    }, []);
+
+    return data
+};
+
+
+/* leftJoin */
+function leftJoin(left, right, left_id, right_id, col_to_join) {
+    var result = [];
+    _.each(left, function (litem) {
+        var f = _.filter(right, function (ritem) {
+            return ritem[right_id] == litem[left_id] && ritem["date"] == litem["date"];
+        });
+        if (f.length == 0) {
+            f = [{}];
+        }
+        _.each(f, function (i) {
+            var newObj = {};
+            _.each(litem, function (v, k) {
+                newObj[k] = v;
+            });
+            _.each(i, function (v, k) {
+                if(k == col_to_join) {
+                    newObj[k] = v;
+                }
+            });
+            result.push(newObj);
+        });
+    });
+    return result;
+}
